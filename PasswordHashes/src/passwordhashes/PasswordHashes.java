1dee134340196f93ee70fb5655264b8b6e515738
joshuakortje <38233249+joshuakortje@users.noreply.github.com>
2019-03-28 16:54:44 -0400

    Set up almost everything except for the checkHash function.

--- PasswordHashes/src/passwordhashes/PasswordHashes.java
+++ PasswordHashes/src/passwordhashes/PasswordHashes.java
@@ -5,26 +5,90 @@
  */
 package passwordhashes;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author jkort
  */
 public class PasswordHashes {
+    
+    private static Vector<Hash> victims;
 
     /**
      * @param args the command line arguments
+     * args[0] is the name of the file with the hashes we are trying to find
+     * args[1] is the name of the file with the dictionary (ie the Bible)
      */
     public static void main(String[] args) throws NoSuchAlgorithmException {
-        // TODO code application logic here
-        String data = "mean";
+        
+        try {
+            inputHashes(args[0]);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(PasswordHashes.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        /* TESTING PURPOSES
+        for(Hash h: victims){
+            System.out.println(h.getUser() + h.getSalt() +h.getHash());
+        }*/
+        
+        
         MessageDigest md = MessageDigest.getInstance("MD5");
         
-        String test = hash(data, md);
+        //counter for counting hashes
+        long count = 0;
         
-        System.out.println(data);
+        //open a scanner for the dictionary
+        Scanner dictionary;
+        String nextWord;
+        try {
+            dictionary = new Scanner(new File(args[1]));
+            while(dictionary.hasNext()){
+                nextWord = dictionary.next();
+                
+                //call function to do the actual hashing and checking
+                checkHashes(nextWord, md);
+                count = count + 1; //keep track of hashes
+                if((count % 1000) == 0){
+                    System.out.println(count);
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(PasswordHashes.class.getName()).log(Level.SEVERE, null, ex);
+        }
+               
+    }
+    
+    public static void checkHashes(String word, MessageDigest md){
+        
+    }
+    
+    public static void inputHashes(String filename) throws FileNotFoundException{
+        victims = new Vector<Hash>();
+        
+        Scanner victimInput = new Scanner(new File(filename));
+        victimInput.useDelimiter(":|\\n");
+        String name = "";
+        String salt = "";
+        String hash = "";
+        
+        //get each row
+        while(victimInput.hasNext()){
+            name = victimInput.next();
+            salt = victimInput.next();
+            hash = victimInput.next();
+            
+            //put into the victim vector
+            victims.add(new Hash(name, salt, hash));
+        }
     }
     
     //this method takes the hash of a word passed in
